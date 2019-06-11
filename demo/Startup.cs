3fb898ef6d8038b056dc9519175b5907fac09b08
Mark Coleman <mark.coleman@iress.co.uk>
2019-06-11 14:53:33 +0100

    Add libs

--- demo/Startup.cs
+++ demo/Startup.cs
@@ -48,7 +48,7 @@ namespace demo
                 app.UseHsts();
             }
 
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
             app.UseStaticFiles();
             app.UseCookiePolicy();
 
